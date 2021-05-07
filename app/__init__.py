babce4ab278681f8918abe4c0694f1d8cac632e1
Icarus <ibuckhold@gmail.com>
2021-05-07 12:59:02 -0500

    set up and check heroku

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,12 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
-
 from .models import db, User, Pantry, Ingredient
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.add_ingredient_routes import ingredient_routes
 from .api.add_pantry_routes import pantry_routes
-
 from .seeds import seed_commands
-
-
 from .config import Config
 
 app = Flask(__name__)
