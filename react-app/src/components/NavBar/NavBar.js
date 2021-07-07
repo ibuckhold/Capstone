4ba77f9634882ce19807662bd714a92914ac42e3
Icarus <ibuckhold@gmail.com>
2021-07-07 13:53:02 -0600

    touching up logout

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -34,9 +34,10 @@ const NavBar = () => {
           <li><NavLink to="/pantries" exact={true} className="icon fas fa-door-closed" activeClassName="active">Pantry</NavLink></li>
           <li><NavLink to='/ingredient/add' exact={true} className="icon fas fa-carrot" activeClassName="active">Ingredients</NavLink></li>
           <li><NavLink to='/recipes' exact={true} className="icon fas fa-scroll" activeClassName="active">Recipe</NavLink></li>
-          <li><LogoutButton className="icon" /></li>
+          <li><LogoutButton className="icon logout" /></li>
         </ul>
         <div className='socialMedia'>
+          <a><i className='fas fa-sign-out-alt'></i></a>
           <a><i className="fab fa-linkedin"></i></a>
           <a><i className="fab fa-github-square"></i></a>
         </div>
